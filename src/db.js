e74f3d8e5b0835e6969e563abc359f34a0a05e82
Giwan Kim <giwan.kim@gmail.com>
2021-09-27 14:49:50 +0900

    fix(kimchi): Change db schema

--- src/db.js
+++ src/db.js
@@ -1,29 +1,30 @@
 const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 
-// connect to db
+// Connect to db
 const db_name = path.join(__dirname, "..", "data", "restaurant.db");
 const db = new sqlite3.Database(db_name, (err) => {
   if (err) {
     console.error(err.message);
     throw err;
   }
-  console.log("Connected to the database 'restaurant.db'");
+  console.log("Connected to 'restaurant.db'");
 });
 
-// create table
+// Create table
 const sql_create = `CREATE TABLE IF NOT EXISTS Restaurant (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(100) NOT NULL,
-  zip VARCHAR(20) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  detail_address VARCHAR(100) NOT NULL,
-  extra_address TEXT,
-  brand VARCHAR(100) NOT NULL,
+  name TEXT NOT NULL,
+  postcode TEXT NOT NULL,
+  address TEXT NOT NULL,
+  detail_address TEXT NOT NULL,
+  type TEXT NOT NULL,
+  brand TEXT NOT NULL,
   area REAL NOT NULL,
   consumption REAL,
-  approved BOOLEAN
+  approved BOOLEAN NOT NULL
 );`;
+
 db.run(sql_create, (err) => {
   if (err) {
     console.error(err.message);
@@ -32,10 +33,10 @@ db.run(sql_create, (err) => {
   console.log("Created table 'Restaurant'");
 
   // populate db with dummy data
-  const sql_insert = `INSERT INTO Restaurant (id, name, zip, address, detail_address, extra_address, brand, area, consumption, approved) VALUES
-    (1, '음식점1', '63309', '제주특별자치도 제주시 첨단로 242', '3층', '지하실', '종가집', 123.45, 1000.5, 'false'),
-    (2, '음식점2', '12345', '도로 한복판', '1층', '옥탑방', 'CJ 푸드', 543.21, 50.1, 'false'),
-    (3, 'restaurant', '90210', 'road address', 'detailed address', 'extra address', 'brand', 12.345, 111, 'false');`;
+  const sql_insert = `INSERT INTO Restaurant (id, name, postcode, address, detail_address, type, brand, area, consumption, approved) VALUES
+    (1, '음식점1', '63309', '제주특별자치도 제주시 첨단로 242', '3층', '양식', '종가집', 123.45, 1000.5, 'false'),
+    (2, '음식점2', '12345', '도로 한복판', '1층', '한식', 'CJ 푸드', 543.21, 50.1, 'false'),
+    (3, 'restaurant', '90210', 'road address', 'detailed address', 'type', 'brand', 12.345, 111, 'false');`;
   db.run(sql_insert, (err) => {
     if (err) {
       console.error(err.message);
