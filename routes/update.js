9ec63939b6562db2fb58bfaeac8f3479dd67f5aa
Giwan Kim <giwan.kim@gmail.com>
2021-10-19 19:19:27 +0900

    feat: Add manufacturer page

--- routes/update.js
+++ routes/update.js
@@ -4,7 +4,6 @@ const adminController = require("../controllers/adminController");
 const router = express.Router();
 
 router.get("/:id", adminController.updateGet);
-
 router.post("/:id", adminController.updatePost);
 
 module.exports = router;
