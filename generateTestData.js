0dad5654fca413eaadcf0f03edc3366e0e829c46
Giwan Kim <giwan.kim@gmail.com>
2021-10-21 17:56:38 +0900

    feat: add script to generate test data

---
+++ generateTestData.js
@@ -0,0 +1,51 @@
+require("dotenv").config({ path: "./envs/.env.development" });
+const faker = require("faker");
+const mongoose = require("mongoose");
+const Restaurant = require("./models/Restaurant");
+const Manufacturer = require("./models/Manufacturer");
+
+// faker.locale = "ko";
+
+// Connect to DB
+mongoose
+  .connect(process.env.DB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB...");
+  });
+
+const main = async () => {
+  for (let i = 0; i < 10; i++) {
+    const restaurant = await Restaurant.create({
+      name: faker.name.findName(),
+      postcode: faker.address.zipCode(),
+      address: faker.address.streetAddress(),
+      detail_address: faker.address.secondaryAddress(),
+      cuisine: "korean",
+      brand: faker.name.findName(),
+      area: faker.finance.amount(),
+      consumption: faker.finance.amount(),
+    });
+
+    const manufacturer = await Manufacturer.create({
+      name: faker.name.findName(),
+      postcode: faker.address.zipCode(),
+      address: faker.address.streetAddress(),
+      detail_address: faker.address.secondaryAddress(),
+      production: faker.finance.amount(),
+    });
+  }
+};
+
+main()
+  .then(() => {
+    console.log("Succefully inserted test data into MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
