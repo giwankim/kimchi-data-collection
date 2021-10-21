1d6daf2324048c2d7b5fba5f8a25f1c77da93841
Giwan Kim <giwan.kim@gmail.com>
2021-10-21 17:50:50 +0900

    refactor: change names of views

--- models/manufacturer.js
+++ models/manufacturer.js
@@ -1,26 +1,19 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const ManufacturerSchema = new Schema({
-  _record_type: { type: String, required: true },
-  _schema_version: { type: String, required: true },
-  composition: {
-    type: String,
-    required: true,
-    enum: ["korean_ingredients", "domestic"],
-  },
-  production_amount: { type: Schema.Types.Decimal128, required: true },
-  province: { type: String },
-  district: { type: String },
-  address: { type: String, required: true },
-  address_detail: { type: String },
-  postal_code: { type: String },
   name: { type: String, required: true },
+  postcode: { type: String },
+  address: { type: String, required: true },
+  detail_address: { type: String },
+  production: { type: Schema.Types.Decimal128, required: true },
+  approved: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
-
-ManufacturerSchema.virtual("display_address").get(() => {});
 
 const Manufacturer = mongoose.model("Manufacturer", ManufacturerSchema);
 
-exports.module = Manufacturer;
+module.exports = Manufacturer;
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,32 +1,23 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const RestaurantSchema = new Schema({
-  _record_type: { type: String, required: true },
-  _schema_version: { type: String, required: true },
-  type_of_kimchi: {
-    type: String,
-    required: true,
-    enum: ["self_made", "purchased"],
-  },
-  composition: {
-    type: String,
-    required: true,
-    enum: ["korean_ingredients", "domestic"],
-  },
-  consumption_amount: { type: Schema.Types.Decimal128, required: true },
-  province: { type: String },
-  district: { type: String },
-  address: { type: String, required: true },
-  address_detail: { type: String },
-  postal_code: { type: String },
   name: { type: String, required: true },
-  area: { type: Schema.Types.Decimal128, required: true },
+  postcode: { type: String },
+  address: { type: String, required: true },
+  detail_address: { type: String, required: true },
   cuisine: {
     type: String,
     required: true,
-    enum: ["korean", "chinese", "japanese", "snack", "other"],
+    enum: ["한식", "중식", "일식", "분식", "기타"],
+  },
+  brand: { type: String, required: true },
+  area: { type: Schema.Types.Decimal128, required: true },
+  consumption: { type: Schema.Types.Decimal128, required: true },
+  approved: {
+    type: Boolean,
+    required: true,
+    default: false,
   },
 });
 
