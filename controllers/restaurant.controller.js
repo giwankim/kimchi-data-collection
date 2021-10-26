3816f3d53332e78f404ad6eeb1ad598c3437f50e
Giwan Kim <giwan.kim@gmail.com>
2021-10-26 17:54:36 +0900

    fix: fix typos; qr code link

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -25,7 +25,7 @@ exports.createRestaurant = (req, res) => {
   res.render("confirm", { model: restaurant });
 };
 
-exports.createRestaurantConfirm = async (req, res) => {
+exports.createRestaurantConfirm = async (req, res, next) => {
   // Create restaurant document
   try {
     const {
@@ -49,24 +49,25 @@ exports.createRestaurantConfirm = async (req, res) => {
       consumption,
     });
     // Generate QR code
-    const url = `https://www.futuresense.co.kr/kimchi/${restaurant._id}`;
+    // const url = `https://www.futuresense.co.kr/kimchi/${restaurant._id}`;
+    const url = `http://3.34.64.241/restaurant/${restaurant._id}/qrinfo`;
     const qrImg = await qr.toDataURL(url);
     res.render("scan", { qrImg });
   } catch (err) {
-    res.status(500).send(err);
+    next(err);
   }
 };
 
-exports.updateRestaurant = async (req, res) => {
+exports.updateRestaurant = async (req, res, next) => {
   try {
     const restaurant = await Restaurant.findById(req.params.id);
     res.render("edit", { model: restaurant });
   } catch (err) {
-    res.status(404).send(err);
+    next(err);
   }
 };
 
-exports.updateRestaurantConfirm = async (req, res) => {
+exports.updateRestaurantConfirm = async (req, res, next) => {
   try {
     const restaurant = {
       ...req.body,
@@ -75,51 +76,48 @@ exports.updateRestaurantConfirm = async (req, res) => {
     await Restaurant.findByIdAndUpdate(req.params.id, restaurant);
     res.redirect("/admin");
   } catch (err) {
-    res.status(500).send(err);
+    next(err);
   }
 };
 
-exports.deleteRestaurant = async (req, res) => {
+exports.deleteRestaurant = async (req, res, next) => {
   try {
     const restaurant = await Restaurant.findById(req.params.id);
     res.render("delete", { model: restaurant });
   } catch (err) {
-    res.status(500).send(err);
+    next(err);
   }
 };
 
-exports.deleteRestaurantConfirm = async (req, res) => {
+exports.deleteRestaurantConfirm = async (req, res, next) => {
   try {
     await Restaurant.findByIdAndDelete(req.params.id);
     res.redirect("/admin");
   } catch (err) {
-    res.status(500).send(err);
+    next(err);
   }
 };
 
-exports.approveRestaurant = async (req, res) => {
+exports.approveRestaurant = async (req, res, next) => {
   try {
     const restaurant = await Restaurant.findById(req.params.id);
     res.render("approve", { model: restaurant });
   } catch (err) {
-    res.status(500).send(err);
+    next(err);
   }
 };
 
-exports.approveRestaurantConfirm = async (req, res) => {
+exports.approveRestaurantConfirm = async (req, res, next) => {
   try {
     await Restaurant.findByIdAndUpdate(req.params.id, { approved: true });
     res.redirect("/admin");
   } catch (err) {
-    res.status(500).send(err);
+    next(err);
   }
 };
 
-exports.generateQrcode = async (req, res) => {
-  /**
-   * TODO!!!
-   */
-  const url = `http://3.34.64.241/restaurant/${req.params.id}/qrinfo`;
+exports.generateQrcode = async (req, res, next) => {
+  const url = `http://3.34.64.241/restaurant/${req.params.id}/qrinfo`; // hardcoded address
   qr.toDataURL(url, (err, qrImg) => {
     if (err) {
       res.status(500).send(err);
@@ -128,11 +126,22 @@ exports.generateQrcode = async (req, res) => {
   });
 };
 
-exports.qrinfo = async (req, res) => {
+exports.qrinfo = async (req, res, next) => {
   try {
     const restaurant = await Restaurant.findById(req.params.id);
     res.render("view-qrcode", { model: restaurant });
   } catch (err) {
-    res.status(404).send(err);
+    next(err);
+  }
+};
+
+exports.getRestaurants = async (req, res, next) => {
+  const perPage = 10;
+  const page = req.params.page || 1;
+  try {
+    const restaurants = await Restaurant.find({});
+    const totalCount = await Restaurant.count();
+  } catch (err) {
+    next(err);
   }
 };
